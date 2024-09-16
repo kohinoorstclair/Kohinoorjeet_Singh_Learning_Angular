825bd0930a7d7ff22ee5c3382adeb0a32cf7d86f
Kohinoor Jeet Singh <w0859774@myscc.ca>
2024-09-16 17:55:39 -0400

    added products in components and updated html and did some css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Product} from "./Shared/Modules/product";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'kohinoorjeet-Singh-Learning-Angular';
-  about = 'hi i AM  computer developer and a fellow tech enthusiast';
-  variable1 = 'hey how is it going';
+  about = 'you can find the best products here';
+
+
+   product1: Product = { productId: '001', name: 'Laptop', price: 1000, brand: 'dell', description: 'High-end laptop' };
+  product2: Product={ productId: '002', name: 'Smartphone', price: 700, brand: 'samsung', description: 'Latest model' };
+product3: Product={ productId: '003', name: 'Headphones', price: 150, brand: 'boat', description: 'Noise-cancelling' };
+product4: Product= { productId: '004', name: 'Monitor', price: 300, brand: 'hp', description: '4K Ultra HD', stock: 10 };
+product5: Product ={ productId: '005', name: 'Keyboard', price: 50, brand: 'hp', description: 'Mechanical keyboard', stock: 20 };
+product6: Product= { productId: '006', name: 'Mouse', price: 30, brand: 'dell', description: 'Wireless mouse' }
+products: Product[] = [this.product1,this.product2,this.product3,this.product4,this.product5,this.product6];
+
 }
