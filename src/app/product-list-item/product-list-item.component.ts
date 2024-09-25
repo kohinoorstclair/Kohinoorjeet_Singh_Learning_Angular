2a66b7769f54c0de3c9a0e401f40e7273edbd6e9
Kohinoor Jeet Singh <w0859774@myscc.ca>
2024-09-24 23:02:32 -0400

    made changes in html for list item

--- src/app/product-list-item/product-list-item.component.ts
+++ src/app/product-list-item/product-list-item.component.ts
@@ -1,10 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {Product} from "../Shared/Modules/product";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-product-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './product-list-item.component.html',
   styleUrl: './product-list-item.component.css'
 })
