9af802f2d7dee05702119ea159041d5a41b1115a
Kohinoor Jeet Singh <w0859774@myscc.ca>
2024-10-01 20:28:00 -0400

    did step 4 and 5 made data content mock and product service

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -16,11 +16,6 @@ import {NgForOf} from "@angular/common";
 })
 export class ProductListComponent {
 
-  products: Product[] = [
-    { productId: 1, name: 'Laptop', price: 1000, brand: 'Brand A', description: 'High performance', stock: 10 , url: "src/images/OIP.jpeg"},
-    { productId: 2, name: 'Phone', price: 600, brand: 'Brand B', description: 'Latest model', url: "src/images/th.jpeg"},
-    { productId: 3, name: 'Headphones', price: 200, brand: 'Brand C', description: 'Noise cancelling', stock: 20,url: "src/images/th (1).jpeg" },
-    { productId: 4, name: 'Monitor', price: 300, brand: 'Brand D', description: '4K display', stock: 5,url: "src/images/th (2).jpeg" }
-  ];
+
 
 }
