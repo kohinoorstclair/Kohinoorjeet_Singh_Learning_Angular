f514dfea28024b5e372d6deda98e4fe206d4c7a3
Kohinoor Jeet Singh <w0859774@myscc.ca>
2024-10-02 18:45:41 -0400

    finished the assighement step 10 to 12 and added some style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,32 @@ import { RouterOutlet } from '@angular/router';
 import {Product} from "./Shared/Modules/product";
 import {NgForOf, NgIf} from "@angular/common";
 import {ProductListComponent} from "./product-list/product-list.component";
+import {ProductListItemComponent} from "./product-list-item/product-list-item.component";
+import {ProductService} from "./services/product.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, NgIf, ProductListComponent],
+  imports: [RouterOutlet, NgForOf, NgIf, ProductListComponent, ProductListItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'kohinoorjeet-Singh-Learning-Angular';
   about = 'you can find the best products here';
+  selectedProduct?: Product;
+  constructor(private productService: ProductService) { }
+  ngOnInit(): void {
+    this.getProductById(2);
+  }
 
+  getProductById(productId: number): void {
+    this.productService.getProductById(productId).subscribe({
+      next: (product) => {
+        this.selectedProduct = product;
+      },
+      error: (err) => console.error("Error fetching product", err)
+    });
+  }
 
 }
--- src/app/data/mock-content.ts
+++ src/app/data/mock-content.ts
@@ -1,8 +1,8 @@
 import {Product} from "../Shared/Modules/product";
 
 export const products: Product[] = [
-  { productId: 1, name: 'Laptop', price: 1000, brand: 'Brand A', description: 'High performance', stock: 10 , url: "src/images/OIP.jpeg"},
-  { productId: 2, name: 'Phone', price: 600, brand: 'Brand B', description: 'Latest model', url: "src/images/th.jpeg"},
-  { productId: 3, name: 'Headphones', price: 200, brand: 'Brand C', description: 'Noise cancelling', stock: 20,url: "src/images/th (1).jpeg" },
-  { productId: 4, name: 'Monitor', price: 300, brand: 'Brand D', description: '4K display', stock: 5,url: "src/images/th (2).jpeg" }
+  { productId: 1, name: 'Laptop', price: 1000, brand: 'dell', description: 'High performance', stock: 10 , url: "src/images/OIP.jpeg"},
+  { productId: 2, name: 'Phone', price: 600, brand: 'hp', description: 'Latest model', url: "src/images/th.jpeg"},
+  { productId: 3, name: 'Headphones', price: 200, brand: 'samsung', description: 'Noise cancelling', stock: 20,url: "images/th (1).jpeg" },
+  { productId: 4, name: 'Monitor', price: 300, brand: 'macitosh', description: '4K display', stock: 5,url: "src/images/th (2).jpeg" }
 ];
