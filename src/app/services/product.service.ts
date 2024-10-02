111b70857ffcf9e4186cbad22eee3407cf40130b
Kohinoor Jeet Singh <w0859774@myscc.ca>
2024-10-01 21:10:47 -0400

    step 8 9 added all the crud operations in the service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,10 +7,43 @@ import {Product} from "../Shared/Modules/product";
   providedIn: 'root'
 })
 export class ProductService {
+  private productList: Product[] = products;
 
   constructor() { }
 
   getProducts(): Observable<Product[]> {
-    return of(products);
+    return of(this.productList);
+  }
+  getProductById(productId: number): Observable<Product | undefined> {
+    const product = this.productList.find(product => product.productId === productId);
+    return of(product);
+  }
+
+  addProduct(newProduct: Product): Observable<Product[]> {
+    this.productList.push(newProduct);
+    return of(this.productList);
+  }
+
+  updateProduct(updatedProduct: Product): Observable<Product[]> {
+    const index = this.productList.findIndex(product => product.productId === updatedProduct.productId);
+    if (index !== -1) {
+      this.productList[index] = updatedProduct;
+    }
+    return of(this.productList);
+  }
+
+  removeProduct(productId: number): Observable<Product | undefined> {
+    const productToRemove = this.productList.find(product => product.productId === productId);
+    this.productList = this.productList.filter(product => product.productId !== productId);
+    return of(productToRemove);
   }
 }
+
+
+
+
+
+
+
+
+
