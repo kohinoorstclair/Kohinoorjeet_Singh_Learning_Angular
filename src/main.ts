33931c91b08494d9a695b2d2ea30d1a813991f11
Kohinoor Jeet Singh <w0859774@myscc.ca>
2024-10-31 17:44:54 -0400

    did step 4 and 5 for assighment 5

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,19 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import { provideRouter, Routes} from "@angular/router";
+
+import { ProductListComponent} from "./app/product-list/product-list.component";
+import { ProductListItemComponent} from "./app/product-list-item/product-list-item.component";
+
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+const routes: Routes = [
+  { path: 'Products', component: ProductListComponent },
+  { path: 'Products/:id', component: ProductListItemComponent }
+];
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).then(r => console.log('Bootstrap successful'))
