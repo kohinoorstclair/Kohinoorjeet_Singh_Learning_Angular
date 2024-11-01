1ac33b1eb795245f6467f975496f70a9f4fdd1b3
Kohinoor Jeet Singh <w0859774@myscc.ca>
2024-11-01 16:38:08 -0400

    assighment 5 complete

--- src/app/data/mock-content.ts
+++ src/app/data/mock-content.ts
@@ -1,8 +1,7 @@
 import {Product} from "../Shared/Modules/product";
-
 export const products: Product[] = [
-  { productId: 1, name: 'Laptop', price: 1000, brand: 'dell', description: 'High performance', stock: 10 , url: "src/images/OIP.jpeg"},
-  { productId: 2, name: 'Phone', price: 600, brand: 'hp', description: 'Latest model', url: "src/images/th.jpeg"},
-  { productId: 3, name: 'Headphones', price: 200, brand: 'samsung', description: 'Noise cancelling', stock: 20,url: "images/th (1).jpeg" },
-  { productId: 4, name: 'Monitor', price: 300, brand: 'macitosh', description: '4K display', stock: 5,url: "src/images/th (2).jpeg" }
+  { productId: 1, name: 'Laptop', price: 1000, brand: 'Dell', description: 'High performance', stock: 10, url: 'OIP.jpeg' },
+  { productId: 2, name: 'Phone', price: 600, brand: 'HP', description: 'Latest model', url: 'th.jpeg' },
+  { productId: 3, name: 'Headphones', price: 200, brand: 'Samsung', description: 'Noise cancelling', stock: 20, url: 'th (1).jpeg' },
+  { productId: 4, name: 'Monitor', price: 300, brand: 'Macintosh', description: '4K display', stock: 5, url: 'th (2).jpeg' }
 ];
