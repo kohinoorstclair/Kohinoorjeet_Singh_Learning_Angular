c2ece7f7e1804778f72613fc3cbab69de1e460bb
Kohinoor Jeet Singh <w0859774@myscc.ca>
2024-11-08 01:57:31 -0500

    fixed some issue wiht product id and other stuff

--- src/app/modify-list-item/modify-list-item.component.ts
+++ src/app/modify-list-item/modify-list-item.component.ts
@@ -29,11 +29,7 @@ export class ModifyListItemComponent implements OnInit {
   ) {
     this.productForm = this.formBuilder.group({
 
-      name: ['', Validators.required],
-      price: [0, [Validators.required, Validators.min(1)]],
-      brand: ['', Validators.required],
-      description: ['', Validators.required],
-      stock: [0, [Validators.required, Validators.min(0)]]
+
     });
   }
 
