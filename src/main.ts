c5c7c11e5baa365115b75266418570c0f76c3cc4
Kohinoor Jeet Singh <w0859774@myscc.ca>
2024-11-15 17:09:51 -0500

    use lazy loading for router paths

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,25 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter, Routes } from "@angular/router";
-import { provideHttpClient } from "@angular/common/http";
-import { importProvidersFrom } from "@angular/core";
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
-import { InMemoryDataService } from "./app/services/in-memory-data.service";
+import { provideRouter, Routes } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './app/services/in-memory-data.service';
 
-import { ProductListComponent } from "./app/product-list/product-list.component";
-import { ModifyListItemComponent } from "./app/modify-list-item/modify-list-item.component";
-import { PageNotFoundComponent } from "./app/page-not-found/page-not-found.component";
 
-// Define the routes
 const routes: Routes = [
-  { path: 'Products', component: ProductListComponent },
-  { path: 'modify-product', component: ModifyListItemComponent },
-  { path: 'modify-product/:id', component: ModifyListItemComponent },
-  { path: 'page-not-found', component: PageNotFoundComponent },
-  { path: '**', component: PageNotFoundComponent } // Wildcard for unmatched routes
+  { path: '', component: AppComponent },
+  { path: 'Products', loadComponent: () => import('./app/product-list/product-list.component').then(m => m.ProductListComponent) },
+  { path: 'modify-product', loadComponent: () => import('./app/modify-list-item/modify-list-item.component').then(m => m.ModifyListItemComponent) },
+  { path: 'modify-product/:id', loadComponent: () => import('./app/modify-list-item/modify-list-item.component').then(m => m.ModifyListItemComponent) },
+  { path: '**', loadComponent: () => import('./app/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent) }
 ];
 
-// Bootstrap the Angular app
+
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(), // HTTP client configuration
-    provideRouter(routes), // Router configuration
-    importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 1000 })) // Use importProvidersFrom
+    provideHttpClient(),
+    provideRouter(routes),
+    importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 1000 }))
   ]
-}).then(r => console.log('Bootstrap successful'))
-  .catch(err => console.error(err));
+}).catch((err) => console.error(err));
