df31bdcc9b03054250406a8f2fc4eed14087241c
Kohinoor Jeet Singh <w0859774@myscc.ca>
2024-11-29 22:12:36 -0500

    Assighemtn 10 added material design to navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,21 @@ import {ProductListComponent} from "./product-list/product-list.component";
 import {ProductListItemComponent} from "./product-list-item/product-list-item.component";
 import {ProductService} from "./services/product.service";
 import { HttpClientModule } from '@angular/common/http';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatAnchor} from "@angular/material/button";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, NgIf, ProductListComponent, ProductListItemComponent, RouterLink],
+  imports: [RouterOutlet, NgForOf, NgIf, ProductListComponent, ProductListItemComponent, RouterLink, MatToolbar, MatAnchor, MatToolbarModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'kohinoorjeet-Singh-Learning-Angular';
+  title = 'electro media';
   about = 'you can find the best products here';
   selectedProduct?: Product;
   constructor(private productService: ProductService) { }
